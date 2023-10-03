ea3e1865180f31d5fa76532ee5cd80383a4636d9
FCAmoroso <francisco200o.junior@hotmail.com>
2023-10-03 10:43:10 -0300

    Aula 0310 setup inicial react

---
+++ React-basic/basic-app/src/components/AppViewer.jsx
@@ -0,0 +1,10 @@
+import styles from "./AppViewer.module.css"
+import React from 'react'
+
+const AppViewer = () => {
+  return (
+    <div className={styles.avContainer}>AppViewer</div>
+  )
+}
+
+export default AppViewer
